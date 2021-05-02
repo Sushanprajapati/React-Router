5094dc143f3a54780df1c0099829f923561f4e02
Sushan <sushanprajapati9808@gmail.com>
2021-05-02 15:56:36 +0545

    Routing and Using links

--- src/App.js
+++ src/App.js
@@ -3,15 +3,28 @@ import './App.css';
 import Nav from './Nav';
 import About from './About';
 import Shop from './Shop';
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
       <Nav />
-      <About />
-      <Shop />
+      {/*Instead of using the About and Shop components directly, we will use route*/}
+      <Switch>
+        <Route path="/" exact component={Home}></Route>
+        <Route path="/about" exact component={About}></Route>
+        <Route path="/shop" exact component={Shop}></Route>
+        </Switch>
     </div>
+    </BrowserRouter>
   );
 }
+
+const Home = () => (
+    <div>
+      <h1>HomePage</h1>
+    </div>
+  )
 
 export default App;
--- src/Nav.js
+++ src/Nav.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import './App.css';
+import {Link} from 'react-router-dom';
 
 function Nav() {
+    const navStyle={
+        color:'white',
+        textDecoration: 'none'
+    }
   return (
-    <div className="App">
+    <div>
       <nav>
             <h3>Logo</h3>
-            <ul>
-                <li>About</li>
-                <li>Shop</li>
+            <ul className = "nav-links">
+                <Link to="/about" style={navStyle}>
+                    <li>About</li>
+                </Link>
+                <Link to="/shop" style={navStyle}>
+                    <li>Shop</li>
+                </Link>
                 </ul>
           </nav>
     </div>
